7a7403aa5cba0407467f6d36372a4dd8abdbf7b3
Francois Planque <github@fplanque.net>
2024-07-08 00:00:55 +0200

    support for descriptive-filenames.md

--- README.md
+++ README.md
@@ -1,22 +1,58 @@
-# obsigo
+# obsigo - Obsidian to hugo bridge
 
-A bridge from Obsidian to Hugo for Static Website Generation.
+A converter/bridge from **[Obsidian](https://obsidian.md)** to **[Hugo](https://gohugo.io)** for Static Website Generation, 
+including **foreverlinks** redirects for **[Netlify](https://www.netlify.com)**.
 
-Run `../obsigo.sh` from your hugo directory.
+This is super-niche but if you are going to use Obsidian + Hugo + Netlify, this will save you so much time you'll want to hug me ;)  
 
-Obsigo will read a source directory (typically `content_src` but could be any location you are managing with Obsidian) 
-and write to a destination directory (typically `content`) that `hugo` will process to generate a static site.
+This is like a v0.1-alpha version but it already handles all of the following...
 
-Obsigo can also generate foreverlinks, typically by writing to `static/_redirects`.
+## Why do we need this?
 
-Obsigo will do the follwoing actions:
+1. Because Obsidian is great for editing and managing a collection of Markdown files and Hugo is great for 
+   generating a static website from Markdown files, BUT:
+   - Hugo works best when all your files are named `index.md` (but in different folders of course) but this makes it 
+     über-painful to search and manage your posts/articles in Obsidian. (Non-descriptive filenames... seriously Hugo?)
+   - Both tools don't agree on the exact same flavor of Markdown (e-g: embedding YouTube videos)
+   - They don't agree on the way links should be made from one file to another (the whole `index.md` thing again)
+2. Hugo can do basic redirects BUT:
+   - They are NOT proper 301 redirects (they are JS redirects, which are bad for SEO)
+   - They are NOT foreverlinks (they only redirect from a single URL to another single URL, not from `*/old_slug` or `*/old_slug/` to `current_canoncial_URL`)
+3. Hugo short tags have limitations (To be addressed in a future version of obsigo)
 
-- Cleanup/Remove unimportant keys from your FrontMatter YAML (IMPORTANT: these changes will be written back to the source directory!)
+## Usage
+
+Run `..path.to../obsigo.sh` from your hugo directory.
+
+Obsigo will read a content source directory (typically `content_src` but could be any location you are managing 
+with Obsidian) and write to a destination directory (typically `content`) that `hugo` will then process to 
+generate a static site.
+
+Obsigo will also generate foreverlinks, typically by writing redirects to `static/_redirects` (those redirects are
+in the format expected by Netlify)
+
+## Features
+
+Obsigo will do the following actions:
+
+### Frontmatter/Metadata/Filenames
+ 
+- Cleanup/Remove unimportant keys from your FrontMatter YAML (IMPORTANT: these changes will be written back 
+  to the source directory!)
 - Collect slugs & aliases from frontmatter `aliases:`, `slug:`, the filename`.md` or the folder_name`/index.md`
   - Generate foreverlinks from the above and save them to a Netlify-compatible `_redirects` file
+  - Also add additional customs redirects from `_redirects_base.txt` (if it exists)
+- Converting obsidian `/cat/pagename.md` to hugo `/cat/pagename/index.md`
+- Automatically handle renamed files. If `/cat/oldname.md` becomes `/cat/newname.md`, obsigo will detect it because the
+  frontmatter `slug:` will still be `oldname`. At that point, obsigo will add `oldname` to the frontmatter `aliases:`
+  and will change he frontmatter `slug:` to `newname`. (This will, as any alias, generate a foreverlink from `*/oldname` to `/cat/newname`.
+
+### Content
+
 - Convert Obsidian-style markup to Hugo-style:
-  - `![TED Talk](https://www.youtube.com/watch?v=M0yhHKWUa0g)` -> `{{< youtube M0yhHKWUa0g >}}`
-  - `![TED Talk](https://youtu.be/M0yhHKWUa0g)` -> `{{< youtube M0yhHKWUa0g >}}`
-- List all markdown links found (for auditing)
+  - YouTube links:
+    - `![TED Talk](https://www.youtube.com/watch?v=M0yhHKWUa0g)` -> `{{< youtube M0yhHKWUa0g >}}`
+    - `![TED Talk](https://youtu.be/M0yhHKWUa0g)` -> `{{< youtube M0yhHKWUa0g >}}`
+- List all Markdown links found (for auditing)
 - List all HTML links found (for auditing)
   - Suggest Markdown equivalents (to be manually applied; useful for cleaning up legacy content)
--- obsigo.py
+++ obsigo.py
@@ -10,72 +10,100 @@ from io import BytesIO
 import re
 
 # Process the frontmatter of the markdown file.
-def process_frontmatter(data, relative_file_path, aliases_dict, stats_dict ):
+def process_frontmatter(src_metadata, relative_file_path, site_aliases_dict, stats_dict ):
     source_changed = False
 
     # Cleanup/Remove unimportant keys:
     for key in unimportant_frontmatter_keys:
-        if key in data:
-            print(f"  Cleanup: Removing key from source '{key}: {data[key]}")
-            del data[key]
+        if key in src_metadata:
+            print(f"  Cleanup: Removing key from source '{key}: {src_metadata[key]}")
+            del src_metadata[key]
             source_changed = True
             stats_dict['frontmatter_source_cleanups'] += 1
 
+
     # Remove 'visibility' key if its value is 'published'
-    if 'visibility' in data and data['visibility'] == 'published':
-        print(f"  Cleanup: Removing key 'visibility': {data['visibility']}")
-        del data['visibility']
+    if 'visibility' in src_metadata and src_metadata['visibility'] == 'published':
+        print(f"  Cleanup: Removing key 'visibility': {src_metadata['visibility']}")
+        del src_metadata['visibility']
         source_changed = True
         stats_dict['frontmatter_source_cleanups'] += 1
 
-    # Collect aliases
-    if 'aliases' in data:
+
+    # Collect aliases:
+    if 'aliases' in src_metadata:
         # Make a copy of the aliases
-        aliases = data['aliases'].copy()
+        post_collected_aliases = src_metadata['aliases'].copy()
         # make sure it is a list
-        if not isinstance(aliases, list):
-            aliases = [aliases]
-        stats_dict['aliases_collected'] += len(aliases)
+        if not isinstance(post_collected_aliases, list):
+            post_collected_aliases = [post_collected_aliases]
+        stats_dict['aliases_collected'] += len(post_collected_aliases)
     else:
-        aliases = []
+        post_collected_aliases = []
 
 
     # Also add the last directory of the file path (not the filename) as an alias
-    folder_names = relative_file_path.split('/')
-    if len(folder_names) >= 2:
-        main_slug = folder_names[-2]
-        aliases.append(main_slug)
-        stats_dict['slugs_collected'] += 1
-    elif len(folder_names) == 1 and 'index.md' not in folder_names[0] :
-        # remove .md from the end of the folder_name
-        main_slug = re.sub(r'\.md$', '', folder_names[0])
-        aliases.append(main_slug)
-        stats_dict['slugs_collected'] += 1
+    split_path = relative_file_path.split('/')
+    if split_path[-1] == 'index.md' or split_path[-1] == '_index.md':
+        # We are in a leaf directory if we have a parent:
+        if len(split_path) >= 2:
+            # we have a parent: add the last directory of the file path (not the filename) as an alias
+            main_slug = split_path[-2]
+            post_collected_aliases.append(main_slug)
+            stats_dict['slugs_collected'] += 1
+        else:
+            # no parent
+            main_slug = None
     else:
-        main_slug = None
+        # We are not in a leaf directory
+        # Use the last part of the file path as the main slug
+        # remove .md from the end of the folder_name
+        main_slug = re.sub(r'\.md$', '', split_path[-1])
+        post_collected_aliases.append(main_slug)
+        stats_dict['slugs_collected'] += 1
 
     # Also add the slug as an alias if it's different from the main slug extracted from the file path
-    if 'slug' in data and data['slug'] != main_slug:
-        aliases.append(data['slug'])
-        stats_dict['divergent_slugs_collected'] += 1
+    if 'slug' in src_metadata and str(src_metadata['slug']) != main_slug:
+        # DIVERGENT slug found!
+        # We need to roll the slugs:
+        print(f"    Divergent slugs found: '{src_metadata['slug']}' != '{main_slug}'")
+        # Old slug must become an alias:
+        post_collected_aliases.append(src_metadata['slug'])
+        stats_dict['divergent_slugs_fixed'] += 1    # Old slug becomes an alias and filename becomes new slug
+        # add to the original aliasses if not already there:
+        if not 'aliases' in src_metadata:
+            src_metadata['aliases'] = []
+        if src_metadata['slug'] not in src_metadata['aliases']:
+            src_metadata['aliases'].append(src_metadata['slug'])
+        # the new slug must become the canoncial slug:
+        src_metadata['slug'] = main_slug
+        source_changed = True
 
-    # remove _?index.md$ from the end of the canonical_uri:
-    canonical_uri = '/' + re.sub(r'_?index\.md$', '', relative_file_path)
+
+    # remove /_?index.md$ from the end of the canonical_uri:
+    # print( f"    Relative file path: {relative_file_path}")
+    canonical_uri = re.sub(r'/_?index\.md$', '/', '/'+relative_file_path)
+    # print( f"    canonical_uri: {canonical_uri}")
     # If there is still a trailing .md, remove it (can happen for search.md for example)
-    canonical_uri = re.sub(r'\.md$', '', canonical_uri)
+    canonical_uri = re.sub(r'\.md$', '/', canonical_uri)
+    # print( f"    canonical_uri: {canonical_uri}")
 
-    for alias in aliases:
-        # Keep only the last part of the alias
-        alias = alias.split('/')[-1]
 
-        if alias in aliases_dict:
+    for alias in post_collected_aliases:
+        # Make sure alias is a string
+        alias = str(alias)
+        # Keep only the last part of the alias --> NO, we actually want to handle aliases like STMag/index.html
+        # alias = alias.split('/')[-1]
+
+        if alias in site_aliases_dict:
             print(f"!!!WARNING!!! Alias '{alias}' already exists in the dictionary.")
         else:
-            aliases_dict[alias] = canonical_uri
+            site_aliases_dict[alias] = canonical_uri
             print(f"  Added alias {alias}->{canonical_uri} to the dictionary.")
             stats_dict['foreverlinks_collected'] += 1
 
     return source_changed
+
 
 
 # Extract and print all links from the markdown content:
@@ -83,19 +111,25 @@ def extract_links(content, file_path):
     # Initialize the Hugo content with the original content
     hugo_content = content
 
-    markdown_links = re.findall(r'(!?\[.*?\]\((.*?)\))', content)
+    markdown_links = re.findall(r'(!?\[(.*?)\]\((.*?)\))', content)
     if markdown_links:
         print(f"  MD Links found in {file_path}:")
         for match in markdown_links:
             print(f"    - {match[0]}")
+
             # Check if the link is a YouTube embed and convert to Hugo tag
-            if match[0].startswith('!') and ('youtube.com/watch' in match[1] or 'youtu.be/' in match[1]):
-                youtube_id = re.findall(r'(?:https?://(?:www\.)?youtube\.com/watch\?v=|https?://youtu\.be/)([\w-]+)', match[1])
+            if match[0].startswith('!') and ('youtube.com/watch' in match[2] or 'youtu.be/' in match[2]):
+                youtube_id = re.findall(r'(?:https?://(?:www\.)?youtube\.com/watch\?v=|https?://youtu\.be/)([\w-]+)', match[2])
                 if youtube_id:
                     hugo_tag = f'{{{{< youtube {youtube_id[0]} >}}}}'
                     print(f"      - Converting YouTube link to Hugo tag: {hugo_tag}")
                     hugo_content = hugo_content.replace(match[0], hugo_tag)
                     stats_dict['youtube_links_converted'] += 1
+
+            # Check if the link ends in index.md and remove it
+            elif match[2].endswith('/index.md'):
+                print(f"      - Removing 'index.md' from the link")
+                hugo_content = hugo_content.replace(match[0], f"[{match[1]}]({match[2].replace('/index.md', '/')})")
 
     # Find HTML links
     # html_links = re.findall(r'(<a\s+(?:[^>]*?\s+)?href=(["\'])(.*?)\2>(.*?)<\/a>)', content)
@@ -111,8 +145,9 @@ def extract_links(content, file_path):
 
     return content, hugo_content
 
+
 # Process a single markdown file:
-def process_file(file_path, relative_file_path, dest_file_path, aliases_dict, stats_dict):
+def process_file(file_path, relative_file_path, dest_root, site_aliases_dict, stats_dict):
 
     stats_dict['source_md_files'] += 1
 
@@ -121,7 +156,7 @@ def process_file(file_path, relative_file_path, dest_file_path, aliases_dict, st
     print( f"  Title: {post['title']}")
 
     # Process the frontmatter
-    source_changed = process_frontmatter(post.metadata, relative_file_path, aliases_dict, stats_dict)
+    source_changed = process_frontmatter(post.metadata, relative_file_path, site_aliases_dict, stats_dict)
 
     # Extract and print links from the content and update the content
     new_src_content, new_hugo_content = extract_links(post.content, relative_file_path )
@@ -129,7 +164,7 @@ def process_file(file_path, relative_file_path, dest_file_path, aliases_dict, st
          post.content = new_src_content
          source_changed = True
 
-    # Save the modified file only if changes were made
+    # Save the modified source file only if changes were made
     if not source_changed:
         print("  SOURCE unchanged.")
     else:
@@ -141,8 +176,25 @@ def process_file(file_path, relative_file_path, dest_file_path, aliases_dict, st
         with open(file_path, 'wb') as output_file:
             output_file.write(f.getvalue())
 
+    # -----------------------
     # Save the new Hugo content to the destination path
     post.content = new_hugo_content
+
+    # Create destination path
+    dest_file_path = os.path.join(dest_root, relative_file_path)
+
+    # Check if the filename ends in _?index.md or search.md
+    if not re.search(r'(/|^)(_?index|search)\.md$', relative_file_path):
+        # Need to make a leaf directory with index.md
+        dest_file_path = re.sub(r'\.md$', '/index.md', dest_file_path)
+        # Need to make sure directory exists:
+
+    # Check if we need to create the directory:
+    dest_dir = os.path.dirname(dest_file_path)
+    if not os.path.exists(dest_dir):
+        print(f"  Creating destination directory: {dest_dir}")
+        os.makedirs(dest_dir)
+
     print(f"  Saving Hugo file: {dest_file_path} ...")
     # Create an in-memory bytes buffer for the Hugo content
     f = BytesIO()
@@ -152,13 +204,36 @@ def process_file(file_path, relative_file_path, dest_file_path, aliases_dict, st
         output_file.write(f.getvalue())
 
 
-# Recursively process all markdown files in a directory:
-def process_directory(source_directory, destination_directory, aliases_dict, stats_dict ):
-    for root, dirs, files in os.walk(source_directory):
-        print()
-        print()
-        print(f"Processing directory: {root} ...")
 
+# Recursively process all markdown files in a directory:
+def process_directory(source_directory, destination_directory, site_aliases_dict, stats_dict ):
+    for root, dirs, files in os.walk(source_directory):
+
+        cur_dirname = os.path.basename(root)
+        if cur_dirname.startswith('.'):
+            # print()
+            # print()
+            # print(f"Skipping hidden directory: {root}")
+            continue
+
+        print()
+        print()
+        print(f"Processing directory: {root} ... files:{files} ... subdirs: {dirs} ...");
+
+        if cur_dirname == '_assets':
+            # Copy the _assets directory to the destination
+            source_assets_path = root
+            relative_assets_path = os.path.relpath(source_assets_path, source_directory)
+            dest_assets_path = os.path.join(destination_directory, relative_assets_path)
+
+            if not os.path.exists(dest_assets_path):
+                print()
+                print(f" Copying _assets directory from {source_assets_path} to {dest_assets_path}")
+                shutil.copytree(source_assets_path, dest_assets_path)
+
+            continue
+
+        # Else this is a regular directory, process the files
         for file in files:
             if file.endswith('.md'):
                 file_path = os.path.join(root, file)
@@ -166,26 +241,7 @@ def process_directory(source_directory, destination_directory, aliases_dict, sta
                 print()
                 print(f" Processing file: {relative_file_path} ...")
 
-                # Create destination path
-                dest_file_path = os.path.join(destination_directory, relative_file_path)
-                dest_dir = os.path.dirname(dest_file_path)
-                if not os.path.exists(dest_dir):
-                    print(f"  Creating destination directory: {dest_dir}")
-                    os.makedirs(dest_dir)
-
-                process_file(file_path, relative_file_path, dest_file_path, aliases_dict, stats_dict)
-
-        # Check for _assets subdirectories and copy them
-        for dir_name in dirs:
-            if dir_name == '_assets':
-                source_assets_path = os.path.join(root, dir_name)
-                relative_assets_path = os.path.relpath(source_assets_path, source_directory)
-                dest_assets_path = os.path.join(destination_directory, relative_assets_path)
-
-                if not os.path.exists(dest_assets_path):
-                    print()
-                    print(f" Copying _assets directory from {source_assets_path} to {dest_assets_path}")
-                    shutil.copytree(source_assets_path, dest_assets_path)
+                process_file(file_path, relative_file_path, destination_directory, site_aliases_dict, stats_dict)
 
 
 if __name__ == "__main__":
@@ -199,7 +255,7 @@ if __name__ == "__main__":
 
 
 
-    aliases_dict = {}
+    site_aliases_dict = {}
 
     # Check if teh source directory exists
     if not os.path.exists(source_directory):
@@ -218,21 +274,21 @@ if __name__ == "__main__":
         'youtube_links_converted': 0,
         'aliases_collected': 0,
         'slugs_collected': 0,
-        'divergent_slugs_collected': 0,
+        'divergent_slugs_fixed': 0, # Old slug becomes an alias and filename becomes new slug
         'foreverlinks_collected': 0,
     }
 
-    process_directory( source_directory, destination_directory, aliases_dict, stats_dict )
+    process_directory( source_directory, destination_directory, site_aliases_dict, stats_dict )
 
     # Display the aliases dictionary in alphabetical order:
-    aliases_dict = dict(sorted(aliases_dict.items()))
+    site_aliases_dict = dict(sorted(site_aliases_dict.items()))
     print("\nAliases dictionary:")
     # for alias, uri in aliases_dict.items():
         # print(f"  {alias} -> {uri}")
-    print(f"  Total aliases: {len(aliases_dict)}")
+    print(f"  Total aliases: {len(site_aliases_dict)}")
     # Write it to a netlify _redirects file:
     with open( dest_redirects_file, 'w') as f:
-        for alias, uri in aliases_dict.items():
+        for alias, uri in site_aliases_dict.items():
             f.write(f"*/{alias} {uri} 301\n")
 
         # Add the contents of ./static/_redirects_base.txt to this file:
